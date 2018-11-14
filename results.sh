0d0db05259104161af2a5b4542de597c73900ca4
W. Ross Morrow <wrossmorrow@stanford.edu>
2018-11-14 10:12:56 -0800

    experiment and result scripts

--- results.sh
+++ results.sh
@@ -6,7 +6,7 @@ PROCESS=( 2 4 6 8 )
 OBSVERS=( 100 1000 10000 100000 1000000 )
 for P in "${PROCESS[@]}" ; do 
 	for N in "${OBSVERS[@]}" ; do 
-		echo "results from job T = ${P}, N = ${N}, K = ${K}"
+		echo "results from job P = ${P}, N = ${N}, K = ${K}"
 		cat "gslregress-${P}-${N}-${K}.txt"
 		echo " "
 	done
