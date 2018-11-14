c9d1586a7f2cce93d523cf4b80c6c2cbb1925885
W. Ross Morrow <wrossmorrow@stanford.edu>
2018-11-13 20:21:44 -0800

    improved compiler flags

--- gslregress.sh
+++ gslregress.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-module load devel openmpi
+module load devel openmpi/3.1.2 icc/2019
+
+CFLAGS="-xHost -O3 -prec-div -no-ftz -restrict"
 
 GSL_SHARED_LIB="/share/software/user/open/gsl/2.3/lib"
 GSL_LIBS="-L${GSL_SHARED_LIB} -lgsl -lgslcblas -lm"
 GSL_INCL="-I/share/software/user/open/gsl/2.3/include"
 
-mpicc gslregress.c -o bin/gslregress ${GSL_LIBS} ${GSL_INCL}
+MPI_INCL=$( mpicc -showme:compile )
+MPI_LIBS=$( mpicc -showme:link )
+
+icc gslregress.c -o bin/gslregress ${CFLAGS} ${GSL_LIBS} ${GSL_INCL} ${MPI_INCL} ${MPI_LIBS}
 if [[ $? -ne 0 ]] ; then exit 1 ; fi
 
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${GSL_SHARED_LIB} ; export LD_LIBRARY_PATH
